7ca15790747513fe1140c6328fe51acd1ee677cc
wildanahb <wildandrdw@gmail.com>
2019-11-11 23:30:08 +0700

    Tugas Modul 3_Kel 24

---
+++ Tugas Modul 3_Kel 24/build/generated-sources/ap-source-output/com/model/Student_.java
@@ -0,0 +1,16 @@
+package com.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-11T23:10:17")
+@StaticMetamodel(Student.class)
+public class Student_ { 
+
+    public static volatile SingularAttribute<Student, Integer> studentId;
+    public static volatile SingularAttribute<Student, String> firstName;
+    public static volatile SingularAttribute<Student, String> lastName;
+    public static volatile SingularAttribute<Student, String> alamat;
+
+}
